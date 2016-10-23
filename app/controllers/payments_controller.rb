c569cf0c5a5a29374eed6861b0b5ebc29b6f4704
nick shook <nicholas.shook@gmail.com>
2016-10-22 17:10:28 -0700

    need live module

--- app/controllers/payments_controller.rb
+++ app/controllers/payments_controller.rb
@@ -1,4 +1,6 @@
 class PaymentsController < ApplicationController
+  include ActionController::Live
+
   def index
     response.headers['Content-Type'] = 'text/event-stream'
     sse = SSE.new(response.stream, retry: 300, event: "event-name")
