7acacc70a777224eb90a105efed076bb44386df5
nick shook <nicholas.shook@gmail.com>
2016-10-22 17:35:21 -0700

    update patch issue cors

--- config/application.rb
+++ config/application.rb
@@ -30,7 +30,7 @@ module Api
     config.middleware.insert_before 0, Rack::Cors do
       allow do
         origins '*'
-        resource '*', :headers => :any, :methods => [:get, :post, :options]
+        resource '*', :headers => :any, :methods => [:get, :post, :options, :patch]
       end
     end
   end
