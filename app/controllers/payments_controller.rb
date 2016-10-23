ebadc4f51be3a7044c4f9dc47db3fb77571de001
nick shook <nicholas.shook@gmail.com>
2016-10-22 17:30:06 -0700

    updated payments

--- app/controllers/payments_controller.rb
+++ app/controllers/payments_controller.rb
@@ -4,10 +4,10 @@ class PaymentsController < ApplicationController
   def index
     response.headers['Content-Type'] = 'text/event-stream'
     sse = SSE.new(response.stream, retry: 300, event: "event-name")
-    sse.write({ name: 'John'})
-    sse.write({ name: 'John'}, id: 10)
-    sse.write({ name: 'John'}, id: 10, event: "other-event")
-    sse.write({ name: 'John'}, id: 10, event: "other-event", retry: 500)
+    loop do
+      sse.write({ name: 'John'})
+      sleep 20
+    end
   ensure
     sse.close
   end
