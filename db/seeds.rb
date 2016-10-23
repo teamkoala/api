bf16fe9ecd1d3809ef439041bf25f18b4bafd4fd
nick shook <nicholas.shook@gmail.com>
2016-10-22 17:20:56 -0700

    no more deletion now is good

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,7 +6,6 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-Item.destroy_all
 Item.find_or_create_by!(name: 'Coffee::large', price: 200, count: 3)
 Item.find_or_create_by!(name: 'Coffee::medium', price: 100, count: 2)
 Item.find_or_create_by!(name: 'Coffee::small', price: 50, count: 5)
